73e53d4d45d9ac14d04291be2bae4eafca14efcd
a1a1a2a4 <a1a1a2a4@gmail.com>
2017-11-01 00:04:41 +0900

    fix 04

--- otani/04.php
+++ otani/04.php
@@ -2,8 +2,10 @@
   $str = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.";
   $str = convertToWord($str);
   $res = array();
+  $condition = [1, 5, 6, 7, 8, 9, 15, 16, 19];
+  $condition = array_map("decrementAll", $condition);
   foreach($str as $i => $word){
-    if($i != (1 || 5 || 6 || 7 || 8 || 9 || 15 || 16 || 19)){
+    if(in_array($i, $condition)){
       $res[$i] = $word[0];
     }else{
       $res[$i] = $word[0]. $word[1];
@@ -12,10 +14,14 @@
   foreach($res as $element){
     echo $element. " ";
   }
+
   function convertToWord($str){
     $str_rep1 = str_replace(",", "", $str);
     $str_rep2 = str_replace(".", "", $str_rep1);
     $word = explode(" ", $str_rep2);
     return $word;
+  }
+  function  decrementAll($num){
+    return($num - 1);
   }
 ?>
