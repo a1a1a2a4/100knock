43aa26cec01dd73934b2aace008efc1c7279d223
a1a1a2a4 <a1a1a2a4@gmail.com>
2017-11-03 20:23:39 +0900

    16

--- otani/16.php
+++ otani/16.php
@@ -1,20 +1,30 @@
 <?php
   $num = trim(fgets(STDIN));
   $file = (file(__DIR__ . '/hightemp.txt'));
-  $line = count($file) / $num;
+  $line = floor(count($file) / $num);
   $rest = count($file) % $num;
-  print($line);
+  $cnt = $num - (($num * ($line + 1)) - count($file));
 
   $n = 1;
-  $count = 0;
-  foreach(range(0, count($file) - 1) as $i){
+  for($i = 0; $i <= count($file) - 1; $i++){
+    print($i);
     file_put_contents("divide-hightemp-". $n. ".txt", $file[$i]. PHP_EOL, FILE_APPEND);
-    $count++;
-    if($rest != 0 && $n >= $num - 1){
-
-    }elseif($count >= $line){
+    if($rest != 0){
+      if((($i + 1) % $line) == 0 && $i != 0){
+        $n++;
+        if($cnt > 0){
+          $n--;
+          for($j = 1; $j <= ((($line + 1) * $n) - ($i + 1)); $j++){
+            file_put_contents("divide-hightemp-". $n. ".txt", $file[$i + $j]. PHP_EOL, FILE_APPEND);
+          }
+          $n++;
+          $i += ($j - 1);
+          print($i. " ]". PHP_EOL);
+        }
+        $cnt--;
+      }
+    }elseif((($i + 1) % $line) == 0 && $i != 0){
       $n++;
-      $count = 0;
     }
   }
 
