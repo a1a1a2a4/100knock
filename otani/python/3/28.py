74b177e1f01438fcfdf90d427a852a409de6f108
a1a1a2a4 <a1a1a2a4@gmail.com>
2017-11-10 00:02:04 +0900

    28 done

--- otani/python/3/28.py
+++ otani/python/3/28.py
@@ -2,8 +2,18 @@ import gzip
 import json
 import re
 import function
+import pdb
 from pprint import pprint
 
 england = function.summarize20()
 basic_infoOb = function.summarize21_27(england)
-# remove to (*, #, :, ;)
+# remove to ^(*, #, :, ;), {{}}
+deletions = {}
+for key, value in basic_infoOb.items():
+    deletions[key] = re.findall(r'(\{\{[^\{\}]+\}\})', value)
+    if(deletions[key]):
+        for k, deletion in enumerate(deletions[key]):
+            deletions[key][k] = deletion.replace(deletion, deletion.replace('{{', '').replace('}}', ''))
+            basic_infoOb[key] = basic_infoOb[key].replace(deletion, deletions[key][k])
+
+pprint(basic_infoOb)
