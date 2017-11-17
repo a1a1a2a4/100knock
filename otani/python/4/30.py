7a153867e6eba9bc207c64e3c397958c25b58a92
a1a1a2a4 <a1a1a2a4@gmail.com>
2017-11-17 11:32:01 +0900

    mid

--- otani/python/4/30.py
+++ otani/python/4/30.py
@@ -1,21 +1,13 @@
 import MeCab
 from pprint import pprint
 
-mecab = MeCab.Tagger()
-
-file = open('neko.txt', 'r')
+file = open('neko.txt.mecab', 'r')
 text = file.read()
-mecab.parse('')
-node = mecab.parseToNode(text)
-s = ''
-while node:
-    surface = node.surface
-    base = node.feature.split(",")[0]
-    pos = node.feature.split(",")[1]
-    pos1 = node.feature.split(",")[2]
-    # print('{0} , {1}, {2}, {3}'.format(surface, base, pos, pos1))
-    s = s + surface + ', ' + base + ', ' + pos + ', ' + pos1 + '\n'
-    node = node.next
-neko_txt = open('neko.txt.mecab', 'w')
-neko_txt.write(s)
-neko_txt.close()
+
+l = text.split('\n')
+morpheme_dic = [{} for i in range(len(l))]
+morpheme_li = [l[i].split(',') for i in range(len(l))]
+for i in range(len(morpheme_dic)):
+        if(morpheme_li[i][0]):
+            morpheme_dic[i][(morpheme_li[i][1], morpheme_li[i][2], morpheme_li[i][3])] = morpheme_li[i][0]
+pprint(morpheme_dic)
