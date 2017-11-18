f8a5333cc1541db85c74c1a6b8bd7856c5f88864
a1a1a2a4 <a1a1a2a4@gmail.com>
2017-11-18 16:01:06 +0900

    30 形態素ごとのdic作成。

--- otani/python/4/30.py
+++ otani/python/4/30.py
@@ -8,6 +8,6 @@ l = text.split('\n')
 morpheme_dic = [{} for i in range(len(l))]
 morpheme_li = [l[i].split(',') for i in range(len(l))]
 for i in range(len(morpheme_dic)):
-        if(morpheme_li[i][0]):
-            morpheme_dic[i][(morpheme_li[i][1], morpheme_li[i][2], morpheme_li[i][3])] = morpheme_li[i][0]
-pprint(morpheme_dic)
+    if(morpheme_li[i][0]):
+        morpheme_dic[i] = {'surface' : morpheme_li[i][0], 'base' : morpheme_li[i][1], 'pos' : morpheme_li[i][2], 'pos1' : morpheme_li[i][3]}
+# pprint(morpheme_dic)
